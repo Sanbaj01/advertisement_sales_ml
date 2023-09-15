65e7753f5535c83136f8d59ef71230ed58c2cf15
Sanbaj Ansari <sanbajansari01@gmail.com>
2023-09-15 09:43:54 +0545

    error resolved

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import pickle
+import numpy as np
 
-model = pickle.load("models\linear_regression_model.pkl")
+with open(r'C:\Users\vs\Desktop\sales_ml_project\model\linear_regression_model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
-def predict_discounted_price(rating:float, rating_count:int, actual_price:float)->float:
+def predict_discounted_price(tv:float, radio:float, newspaper:float)->float:
     """predicts discounted price based on rating ...
     Args:
 
@@ -11,18 +13,19 @@ def predict_discounted_price(rating:float, rating_count:int, actual_price:float)
     Raise:
     """
     try:
-        input = None # (rating, rating_count, actual_price)
+        #input = None # (rating, rating_count, actual_price)
+        input = np.array([tv, radio, newspaper]).reshape(1,-1)
         predictions = model.predict(input)
         return predictions
-    except:
-        raise ValueError("")
+    except Exception as e:
+        raise ValueError(str(e))
     
 
 if __name__ == "__main__":
-    rating = input("rating: ")
-    rating_count = input("rating count: ")
-    actual_price = input("actual price: ")
+    tv = float(input("TV : "))
+    radio = float(input("Radio: "))
+    newspaper = float(input("Newspaper: "))
 
-    predicted_discount = predict_discounted_price(rating, rating_count, actual_price)
+    predicted_discount = predict_discounted_price(tv, radio, newspaper)
 
     print(predicted_discount)
